6e7bd213f907971538bc0f785de3816d0a13b0ba
benny70712 <klt.kyawlintun2023@gmail.com>
2025-04-04 12:18:59 -0400

    Created a new frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import RegisterPage from './RegisterPage';
-import LoginPage from './LoginPage';
 
 function App() {
+
+
   return (
-    <Router>
-      <nav>
-        <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <div>
+        Hello world
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
